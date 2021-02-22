94f71f83424b9ebcf83414a898f4cd5af9642767
No.5972 <32865305+No5972@users.noreply.github.com>
2021-02-22 23:25:53 +0800

    Create baiduICR.py

---
+++ baiduICR.py
@@ -0,0 +1,21 @@
+#导入VcrClient配置文件
+import vcr_sample_conf 
+
+#导入VCR相关模块
+from baidubce import exception
+from baidubce.services import vcr
+from baidubce.services.vcr.vcr_client import VcrClient
+
+import os
+
+vcr_client = VcrClient(vcr_sample_conf.config)
+preset = ""
+
+path = os.getcwd()
+get_dir = os.listdir(path)
+for i in get_dir:
+	if i.endswith('.jpg') or i.endswith('.png'):
+		source = "https://imageproxy.pimg.tw/resize?url=https://raw.githubusercontent.com/No5972/pixiv-github-action/runner/87899740_2B.png"
+		response = vcr_client.put_image(source, preset)
+		if response.label == 'REJECT': 
+			os.remove(i)
