6ebfa9abb0c240c535bc942f7e369956e7856ebc
No.5972 <32865305+No5972@users.noreply.github.com>
2021-02-22 23:36:53 +0800

    Update baiduICR.py

--- baiduICR.py
+++ baiduICR.py
@@ -7,6 +7,7 @@ from baidubce.services import vcr
 from baidubce.services.vcr.vcr_client import VcrClient
 
 import os
+from urllib import parse
 
 vcr_client = VcrClient(vcr_sample_conf.config)
 preset = ""
@@ -15,7 +16,7 @@ path = os.getcwd()
 get_dir = os.listdir(path)
 for i in get_dir:
 	if i.endswith('.jpg') or i.endswith('.png'):
-		source = "https://imageproxy.pimg.tw/resize?url=https://raw.githubusercontent.com/No5972/pixiv-github-action/runner/" + i
+		source = "https://imageproxy.pimg.tw/resize?url=https://raw.githubusercontent.com/No5972/pixiv-github-action/runner/" + parse.quote(i)
 		response = vcr_client.put_image(source, preset)
 		if response.label == 'REJECT': 
 			print(i + ' BAD')
