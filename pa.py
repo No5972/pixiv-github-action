01f4be3b39e6df1709005b910daa2aeee20d4cf5
No.5972 <32865305+No5972@users.noreply.github.com>
2021-02-25 08:43:36 +0800

    Update pa.py

--- pa.py
+++ pa.py
@@ -9,23 +9,30 @@ headers = {
 
 path = './'
 repeat = 1
+repeat_user_name = 1
 
 
 def getSinglePic(url):
     global repeat
+    global repeat_user_name
     response = requests.get(url, headers=headers)
     # 提取图片名称
     name = re.search('"illustTitle":"(.+?)"', response.text)
     name = name.group(1)
     illust_id = re.search('"illustId":"(.+?)"', response.text)
     illust_id = illust_id.group(1)
+    user_name = re.search('"userName":"(.+?)"', response.text)
+    user_name = user_name.group(1)
     if re.search('[\\\ \/ \* \? \" \: \< \> \|]', name) != None:
         name = re.sub('[\\\ \/ \* \? \" \: \< \> \|]', str(repeat), name)
         repeat += 1
+    if re.search('[\\\ \/ \* \? \" \: \< \> \|]', user_name) != None:
+        user_name = re.sub('[\\\ \/ \* \? \" \: \< \> \|]', str(repeat_user_name), user_name)
+        repeat_user_name += 1
     # 提取图片原图地址
     picture = re.search('"original":"(.+?)"},"tags"', response.text)
     pic = requests.get(picture.group(1), headers=headers)
-    f = open(path + '%s_%s.%s' % (illust_id, name, picture.group(1)[-3:]), 'wb')
+    f = open(path + '%s_%s-by-%s.%s' % (illust_id, name, user_name, picture.group(1)[-3:]), 'wb')
     f.write(pic.content)
     f.close()
 
